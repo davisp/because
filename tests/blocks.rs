b983a54c8a9bb3a05f96857b4b6e89487b157392
Paul J. Davis <paul.davis@tiledb.com>
2025-02-25 15:12:16 -0600

    Use a struct instead of single variant enum

--- tests/blocks.rs
+++ tests/blocks.rs
@@ -1,5 +1,12 @@
 use because::Because;
 
+/// The main BlocksError for a hypothetical Blocks.txt parsing crate
+///
+/// This would be in something like src/lib.rs that encapsulates all of the
+/// top level error types returned by the library.
+///
+/// Note that each variant is marked transparent as the first line of an error
+/// report being "An error in blocks.rs occurred!" isn't super useful.
 #[derive(Debug, thiserror::Error)]
 pub enum BlocksError {
     #[error(transparent)]
@@ -9,6 +16,10 @@ pub enum BlocksError {
     FromFile(#[from] FromFileError),
 }
 
+/// An error encountered while parsing a Blocks.rs that was downloaded
+///
+/// We have errors issuing the request, reading the response, and parsing
+/// the response.
 #[derive(Debug, thiserror::Error)]
 pub enum DownloadError {
     #[error("Request error while downloading Blocks.txt")]
@@ -18,25 +29,37 @@ pub enum DownloadError {
     ReadBody(#[source] std::io::Error),
 
     #[error("Error parsing Blocks.txt")]
-    Parse(#[from] ParserError),
+    Parse(#[from] ParseError),
 }
 
+/// An error encountered while parsing a random file on disk
+///
+/// We have errors reading the file (i.e., it doesn't exist) and also the
+/// same ParseError when parsing the loaded data.
 #[derive(Debug, thiserror::Error)]
 pub enum FromFileError {
     #[error("Error reading Blocks.txt from {0}")]
     ReadFile(std::path::PathBuf, #[source] std::io::Error),
 
     #[error("Error parsing Blocks.txt from {0}")]
-    Parse(std::path::PathBuf, #[source] ParserError),
+    Parse(std::path::PathBuf, #[source] ParseError),
 }
 
+/// An error encounterd while parsing
+///
+/// This represents that a parse error happened at some location in Blocks.txt
+/// and carries the type of parse error encountered.
 #[derive(Debug, thiserror::Error)]
-pub enum ParserError {
-    #[error("Invalid data on line {0}")]
-    InvalidData(usize, #[source] ParseErrorReason),
+#[error("Invalid data on line {line}")]
+pub struct ParseError {
+    line: usize,
+    #[source]
+    kind: ParseErrorKind,
 }
+
+/// The kind of ParseError that was encountered.
 #[derive(Debug, thiserror::Error)]
-pub enum ParseErrorReason {
+pub enum ParseErrorKind {
     #[error("missing semicolon")]
     NoSemicolon,
 
@@ -52,7 +75,10 @@ fn some_test() {
     let root = "foo".parse::<u32>().unwrap_err();
     let err = BlocksError::FromFile(FromFileError::Parse(
         std::path::PathBuf::from("path/to/Blocks.txt"),
-        ParserError::InvalidData(5, ParseErrorReason::ParseInt(root)),
+        ParseError {
+            line: 5,
+            kind: ParseErrorKind::ParseInt(root),
+        },
     ));
 
     let mesg = format!("{}\n{}", err, err.because().unwrap());
